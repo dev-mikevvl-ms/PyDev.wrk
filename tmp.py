21e3575499e51e87aebd66f2b9ae51aa52da47de
mikev@W10HLEN <devel.mikevvl@gmail.com>
2022-01-24 07:57:30 +0400

    Beg:02

---
+++ tmp.py
@@ -0,0 +1,4 @@
+import sys, site
+print('sys:', sys.version, sys.version_info, sys.api_version, sys.prefix, sys.exec_prefix, '\nsite:',
+    site.getsitepackages(), site.getusersitepackages(), site.getuserbase(), sep='\n')
+    
